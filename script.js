57248adfb382be77ba0059787291007df55aa289
Kushal-deep <kushaldeep448@gmail.com>
2025-01-12 13:51:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 function stringChop(str, size) {
   // your code here
+
+	let n=str.length;
+	let ans=[]
+	let i=0;
+	while(i<n){
+		let t='';
+		let c=0;
+		while(i<n && c<size){
+			t=t+str[i]
+			i++
+			c++
+		}
+		ans.push(t)
+		
+}
+	return ans  
 }
 
 // Do not change the code below
